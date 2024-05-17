7379147727a5ca16c12d363297d1989ac9dbeb57
APStrickland <3274959+APStrickland@users.noreply.github.com.>
2024-05-17 17:11:16 -0600

    Add all the missing code files... I failed at copy/pasting?!

---
+++ Tests/IOInfoExtensions.PowerShell.Tests/Helpers/PSTestBase.cs
@@ -0,0 +1,33 @@
+﻿using IOInfoExtensions.TestUtilities;
+using System;
+using System.IO;
+
+namespace IOInfoExtensions.PowerShell.Tests.Helpers
+{
+    public class PSTestBase : TestBase
+    {
+        public readonly string modulePath;
+        public readonly Version psVersion;
+
+        public PSTestBase() : base()
+        {
+            modulePath = Path.Combine(testRootDirectory.Parent.FullName, "Module", "IOInfoExtensions.psd1");
+
+#if PSV74
+            psVersion = new Version(7, 4);
+#elif PSV73
+            psVersion = new Version(7, 3);
+#elif PSV72
+            psVersion = new Version(7, 2);
+#elif PSV51
+            psVersion = new Version(5, 1);
+#endif
+
+            var version = PowerShellHelper.GetPowerShellVersion();
+            if (!version.StartsWith($"{psVersion.Major}.{psVersion.Minor}"))
+            {
+                throw new Exception($"Using the wrong version of PowerShell. Expected {psVersion} but got {version}");
+            }
+        }
+    }
+}
---
+++ Tests/IOInfoExtensions.PowerShell.Tests/Helpers/PowerShellExecutionResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace IOInfoExtensions.PowerShell.Tests.Helpers
+{
+    internal class PowerShellExecutionResults
+    {
+        internal List<ErrorRecord> Errors { get; set; } = new List<ErrorRecord>();
+
+        internal List<IOInfoResults> Items { get; set; } = new List<IOInfoResults>();
+    }
+
+    internal class IOInfoResults
+    {
+        internal string Purpose { get; set; }
+        internal string TypeName { get; set; }
+        internal string FullName { get; set; }
+        internal bool Exists { get; set; }
+        internal Version PSVersion { get; set; }
+    }
+}
